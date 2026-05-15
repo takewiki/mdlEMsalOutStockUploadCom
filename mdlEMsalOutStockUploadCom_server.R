5786c1f0a716ef361295837fb4800862076acb6c
takewiki <hulilei@takeiwki.com.cn>
2026-05-15 05:27:41 +0000

    v1.0.0

---
+++ mdlEMsalOutStockUploadCom_server.R
@@ -0,0 +1,5 @@
+
+mdlEMsalOutStockUploadServer::salOutStockServer(input = input,output = output,session = session,dms_token = dms_token,erp_token = erp_token)
+
+  
+
---
+++ mdlEMsalOutStockUploadCom_ui.R
@@ -0,0 +1,25 @@
+#tabName need to be update
+#tabBox title need to be update
+#tabBox id need to be update
+tabItem(tabName = "mdlEMsalOutStockUploadCom",
+                    fluidRow(
+                      column(width = 12,
+                             tabBox(title ="数据中台模块开发DEMO工作台",width = 12,
+                                    id='tabSet_mdlEMsalOutStockUploadCom',height = '300px',
+                                    #以上为注册ui组件的内容-----
+                                    #begin----
+                                    #uiComponent seperated by comma (,)
+                                    mdlEMsalOutStockUploadUI::salOutStockUI()
+                                    
+                                    
+                                    #end----
+                                    
+                                    
+                                    
+                                    
+                                    
+                             )
+                      )
+                    )
+)
+ 
